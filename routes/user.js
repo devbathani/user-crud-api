52071a9c1966cf4db53530200c1f3ecf0e6fb8d5
devbathani <devbathani@credicxo.com>
2022-12-16 14:37:31 +0530

    Delete Request implemented

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {v4 as uuidv4} from 'uuid'
 const router = express.Router();
 
-const userData = [
+let userData = [
 ]
 
 router.get('/', (req,res) => {
@@ -12,8 +12,23 @@ router.get('/', (req,res) => {
 router.post('/', (req, res) =>{
     const user = req.body;
     userData.push({...user, id: uuidv4()});
-    res.send(`Username : ${user.name}`);
+    res.send(`User id : ${user.id}`);
 },);
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+
+    const userDataById = userData.find((user) => user.id == id);
+    res.send(userDataById);
+})
+
+router.delete('/:id', (req, res)=> {
+  const { id } = req.params;
+   userData = userData.filter((user) => user.id != id);
+  res.send(`User is deleted ${id}`);
+});
+
+
 
 export default router;
 
